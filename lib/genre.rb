8e2f1227017434000538b1f554999bba43bd31b4
github username <github email address>
2015-10-15 00:39:57 -0400

    artist

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,23 @@
 # code genre here
+class Genre
+  GENRES = []
+  attr_accessor :name, :songs, :artists
+  def initialize
+    @songs = []
+    @artists = []
+    GENRES << self
+  end
+
+  def self.all
+    GENRES
+  end
+
+  def self.reset_genres
+    GENRES.clear
+  end
+
+  def self.count
+    GENRES.count
+  end
+
+end
--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,14 @@
 # code song here
+require "pry"
+class Song
+  attr_accessor :name, :artist
+  attr_reader :genre
+  def initialize
+    @genre = ""
+  end
+
+  def genre=(name)
+    @genre = name
+    @genre.songs << self
+  end
+end
