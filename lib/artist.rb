f18c7471f8daeb466b00f81273ed53a4ee2bdc98
Chris Aporta <christopher.aporta@gmail.com>
2015-10-26 01:20:40 -0400

    complete lab

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,34 @@
-# code artist here
+require 'pry'
+
+class Artist
+  @@artists = []
+
+  attr_accessor :name, :songs, :genres
+
+  def initialize
+    @songs = []
+    @genres = []
+    @@artists << self
+  end
+
+  def add_song(song)
+    @songs << song
+    genres << song.genre
+    song.artist = self
+    unless song.genre == nil || song.genre.artists.include?(self)
+      song.genre.artists << self
+    end
+  end
+
+  def self.all
+    @@artists
+  end
+
+  def self.count
+    all.length
+  end
+
+  def self.reset_artists
+    all.clear
+  end
+end
--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,25 @@
-# code genre here
+require 'pry'
+
+class Genre
+  @@genres = []
+
+  attr_accessor :name, :songs, :artists
+
+  def initialize
+    @songs = []
+    @artists = []
+    @@genres << self
+  end
+
+  def self.all
+    @@genres
+  end
+
+  def self.count
+    all.length
+  end
+
+  def self.reset_genres
+    all.clear
+  end
+end
