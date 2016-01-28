35918789e9bce0002be9cf5105fc9ea7d284af1d
halfields <hfields697@aol.com>
2016-01-28 02:39:39 +0000

    done

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,37 @@
-# code artist here
+class Artist
+
+  attr_accessor :songs, :name
+
+  @@artists = []
+
+  def Artist.reset_artists
+    @@artists.clear
+  end
+
+  def Artist.count
+    @@artists.count
+  end
+
+
+  def Artist.all
+    @@artists
+  end
+
+  def initialize
+    @name = name
+    @songs = []
+    @@artists << self
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def genres
+    @genre = self.songs.collect  {|song| song.genre}
+  end
+
+end
+
+
