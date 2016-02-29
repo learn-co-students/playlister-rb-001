3a2f1045ef03bec2d62c7ad0a01e1c8a8c50efd8
thaik1618 <kthai1618@gmail.com>
2016-03-01 00:38:20 +0100

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,39 @@
-# code artist here
+require 'pry'
+
+class Artist
+
+  @@artists = []
+  
+  attr_accessor :songs, :name
+
+  def initialize
+    @songs = []
+    @@artists.push(self)
+  end
+
+  def add_song(song)
+    @songs.push(song)
+    song.artist = self
+  end
+
+  def genres
+    array = []
+    @songs.each do |song|
+      array.push(song.genre)
+    end
+    array
+  end
+
+  def self.reset_artists
+    @@artists = []
+  end
+
+  def self.all
+    @@artists
+  end
+
+  def self.count
+    @@artists.count
+  end
+
+end
--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,13 @@
-# code song here
+class Song
+
+  attr_accessor :artist, :name
+  attr_reader :genre
+
+  def initialize
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.songs.push(self)
+  end
+end
