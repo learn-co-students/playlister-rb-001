b55d44edaa09ed8d736990c83318dc78cd56cded
fhernandez173 <f.hernandez173@gmail.com>
2016-07-11 11:44:44 -0400

    Classes relation

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,17 @@
-# code song here
+require 'pry'
+
+class Song
+
+  attr_accessor :name, :artist
+  attr_reader :genre
+
+  def initialize
+
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self
+  end
+
+end
