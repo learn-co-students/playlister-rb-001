0f963e6ab39d0723df485475a5d258490ea9b4c2
jomar <jomar.epr@gmail.com>
2016-11-07 10:26:10 -0500

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,32 @@
-# code artist here
+class Artist
+
+  attr_accessor :name, :songs, :genres
+
+  ARTISTS = []
+
+  def initialize
+    @songs = []
+    @genres = []
+    ARTISTS << self
+  end
+
+  def add_song(song)
+    self.songs << song
+    song.artist = self
+    self.genres << song.genre
+    song.genre.artists << self if song.genre && !song.genre.artists.include?(self)
+  end
+
+  def self.reset_artists
+    ARTISTS.clear
+  end
+
+  def self.count
+    self.all.count
+  end
+
+  def self.all
+    ARTISTS
+  end
+
+end
--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,12 @@
-# code song here
+class Song
+
+  attr_reader :genre
+  attr_accessor :name, :artist
+
+  def genre=(genre)
+    #why doesn't self work here
+    @genre = genre
+    genre.songs << self
+  end
+
+end
